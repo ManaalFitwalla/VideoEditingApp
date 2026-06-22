af26a17c1c6ff32921eba6987509a8b6162730bd
BhatiAafiya <bhati.aafiya32@gmail.com>
2026-06-23 00:11:25 +0530

    done

--- app/src/main/java/com/example/fabcut/GalleryActivity.kt
+++ app/src/main/java/com/example/fabcut/GalleryActivity.kt
@@ -1,8 +1,10 @@
 package com.example.fabcut
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -11,22 +13,59 @@ class GalleryActivity : AppCompatActivity() {
 
     private lateinit var recyclerView: RecyclerView
     private lateinit var adapter: MediaAdapter
+    private lateinit var txtSelectedCount: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_gallery)
 
-        recyclerView = findViewById(R.id.recyclerView)
+        txtSelectedCount =
+            findViewById(R.id.txtSelectedCount)
 
-        recyclerView.layoutManager = GridLayoutManager(this, 3)
+        recyclerView =
+            findViewById(R.id.recyclerView)
+
+        recyclerView.layoutManager =
+            GridLayoutManager(this, 3)
+
+        recyclerView.addItemDecoration(
+            ItemSpacingDecoration(8)
+        )
 
         val mediaType =
             intent.getStringExtra("MEDIA_TYPE") ?: "PHOTO"
 
         val mediaList = loadMedia(mediaType)
 
-        adapter = MediaAdapter(mediaList)
+        adapter = MediaAdapter(
+
+            mediaList,
+
+            { mediaItem ->
+
+                val intent =
+                    Intent(this, PreviewActivity::class.java)
+
+                intent.putExtra(
+                    "MEDIA_URI",
+                    mediaItem.uri.toString()
+                )
+
+                intent.putExtra(
+                    "IS_VIDEO",
+                    mediaItem.isVideo
+                )
+
+                startActivity(intent)
+            },
+
+            { count ->
+
+                txtSelectedCount.text =
+                    "$count Items Selected"
+            }
+        )
 
         recyclerView.adapter = adapter
     }
@@ -65,10 +104,14 @@ class GalleryActivity : AppCompatActivity() {
         cursor?.use {
 
             val idColumn =
-                it.getColumnIndexOrThrow(MediaStore.Files.FileColumns._ID)
+                it.getColumnIndexOrThrow(
+                    MediaStore.Files.FileColumns._ID
+                )
 
             val typeColumn =
-                it.getColumnIndexOrThrow(MediaStore.Files.FileColumns.MEDIA_TYPE)
+                it.getColumnIndexOrThrow(
+                    MediaStore.Files.FileColumns.MEDIA_TYPE
+                )
 
             while (it.moveToNext()) {
 
@@ -76,7 +119,10 @@ class GalleryActivity : AppCompatActivity() {
                 val mediaType = it.getInt(typeColumn)
 
                 val contentUri: Uri =
-                    if (mediaType == MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE) {
+                    if (
+                        mediaType ==
+                        MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE
+                    ) {
 
                         Uri.withAppendedPath(
                             MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
@@ -94,7 +140,8 @@ class GalleryActivity : AppCompatActivity() {
                 mediaList.add(
                     MediaItem(
                         contentUri,
-                        mediaType == MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO
+                        mediaType ==
+                                MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO
                     )
                 )
             }
--- app/src/main/java/com/example/fabcut/MediaAdapter.kt
+++ app/src/main/java/com/example/fabcut/MediaAdapter.kt
@@ -1,5 +1,4 @@
 package com.example.fabcut
-import com.bumptech.glide.Glide
 
 import android.view.LayoutInflater
 import android.view.View
@@ -7,9 +6,16 @@ import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
+import jp.wasabeef.glide.transformations.RoundedCornersTransformation
+import com.bumptech.glide.request.RequestOptions
+import android.widget.Toast
 
 class MediaAdapter(
-    private val mediaList: MutableList<MediaItem>
+    private val mediaList: MutableList<MediaItem>,
+    private val onLongClick: (MediaItem) -> Unit,
+    private val onSelectionChanged: (Int) -> Unit
 ) : RecyclerView.Adapter<MediaAdapter.MediaViewHolder>() {
 
     class MediaViewHolder(itemView: View) :
@@ -45,7 +51,15 @@ class MediaAdapter(
 
         Glide.with(holder.itemView.context)
             .load(mediaItem.uri)
-            .centerCrop()
+            .apply(
+                RequestOptions.bitmapTransform(
+                    RoundedCornersTransformation(
+                        24,
+                        0,
+                        RoundedCornersTransformation.CornerType.ALL
+                    )
+                )
+            )
             .into(holder.imageView)
 
         if (mediaItem.isSelected) {
@@ -63,11 +77,26 @@ class MediaAdapter(
             holder.container.background = null
         }
 
+        // Single click = Select / Unselect
         holder.itemView.setOnClickListener {
 
-            mediaItem.isSelected = !mediaItem.isSelected
+            mediaItem.isSelected =
+                !mediaItem.isSelected
 
             notifyItemChanged(position)
+
+            val selectedCount =
+                mediaList.count { it.isSelected }
+
+            onSelectionChanged(selectedCount)
+        }
+
+        // Long click = Open Preview
+        holder.itemView.setOnLongClickListener {
+
+            onLongClick(mediaItem)
+
+            true
         }
     }
 
