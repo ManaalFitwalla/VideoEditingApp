1e2745e5c38ba2d02d8e404a64c0c336afed050c
Rushda <rushdaa189@gmail.com>
2026-07-06 20:48:44 +0530

    filter added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,12 @@ android {
 }
 
 dependencies {
+    implementation("com.arthenica:ffmpeg-kit-full:6.0-2")
 
+    implementation("jp.co.cyberagent.android:gpuimage:2.1.0")
+    implementation("androidx.media3:media3-effect:1.7.1")
+    implementation("androidx.media3:media3-exoplayer:1.7.1")
+    implementation("androidx.media3:media3-ui:1.7.1")
     implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("jp.wasabeef:glide-transformations:4.3.0")
     implementation("com.github.yalantis:ucrop:2.2.11")
